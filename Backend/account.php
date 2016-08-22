52305c89b45dc71147ebc1f7fd4d1468fbb33904
Macindroid <acaughey@macindroid.com>
2016-08-23 00:51:10 +1000

    Working on PHP Form
    Going to sleep work on Tomorrow

--- Backend/account.php
+++ Backend/account.php
@@ -2,19 +2,33 @@
 // define variables
 $name = $email = $password = $confirmPassword = $yearLevel = $studentNumber = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = testInput($_POST["inputFullName"]);
-  $email = testInput($_POST["inputEmail"]);
-  $password = testInput($_POST["inputPassword"]);
-  $confirmPassword = testInput($_POST["inputConfirmPassword"]);
-  $yearLevel = testInput($_POST["inputYearLevel"]);
-  $studentNumber = testInput($_POST["inputID"])
-}
-
+// Strips unnecessary characters
 function testInput($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
+function console_log( $data ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $data ) .')';
+  echo '</script>';
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = testInput($_POST["inputFullName"]);
+  $email = testInput($_POST["inputEmail"]);
+  $password = testInput($_POST["inputPassword"]);
+  $confirmPassword = testInput($_POST["inputConfirmPassword"]);
+  $yearLevel = testInput($_POST["inputYearLevel"]);
+  $studentNumber = testInput($_POST["inputID"]);
+}
+
+console_log($name);
+console_log($email);
+console_log($password);
+console_log($confirmPassword);
+console_log($yearLevel);
+console_log($studentNumber);
 ?>
